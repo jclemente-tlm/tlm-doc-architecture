2eb9373a725438e6f42b3ed85b866c624781f52e
Jose Clemente <jclemente.tlm@gmail.com>
2025-08-10 02:04:51 -0500

    wip: corrije baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://jclemente-tlm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tlm-doc-architecture/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
